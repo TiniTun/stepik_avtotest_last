2c430fe4a2a628228f6252455e1da2b69b479121
Egor Rydannykh <erydannykh@yandex-team.ru>
2023-01-26 22:46:40 +0300

    add base class

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,11 @@ from selenium.webdriver.common.by import By
 
 class TestMainPage():
 
+    def go_to_login_page(self, browser):
+        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         browser.get(link)
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        self.go_to_login_page(browser)
