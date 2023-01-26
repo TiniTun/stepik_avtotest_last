8f1cad5a2613e3a7d6b8147b389cec4f53c42481
Egor Rydannykh <erydannykh@yandex-team.ru>
2023-01-26 17:45:17 +0300

    add project files

---
+++ conftest.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_language = request.config.getoption("language")
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
