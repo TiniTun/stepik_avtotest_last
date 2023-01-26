b647a6803b24a39921d91be2dd1b31088af76e7c
Egor Rydannykh <erydannykh@yandex-team.ru>
2023-01-26 23:19:58 +0300

    add new logic exeptions

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,10 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
-class TestMainPage():
 
-    def go_to_login_page(self, browser):
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-
-    def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        browser.get(link)
-        self.go_to_login_page(browser)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
